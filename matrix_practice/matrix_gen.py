f0a2f3370a84597e7e426a213bd2cdebe432c221
msjeong97 <hpmltsws@gmail.com>
2021-03-28 18:47:48 +0900

    update to tf2

--- matrix_practice/matrix_gen.py
+++ matrix_practice/matrix_gen.py
@@ -1,21 +1,19 @@
+# if you didn't build tensorflow from source 
+# import os
+# os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 import tensorflow as tf
-import os
-os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 
-# 1 2
-# 2 3
+# [[1 2]	[[1 1]
+# 	2 3]]	  1 1]]
 a = tf.constant([[1, 2], [2, 3]])
-b = tf.constant([[1, 2], [2, 3]])
+b = tf.constant([[1, 1], [1, 1]])
 
-c = tf.add(a, b)
-d = tf.subtract(a, b) 
+add_res = tf.add(a, b)
+sub_res = tf.subtract(a, b) 
+mult_res = tf.multiply(a, b)
+div_res = tf.divide(a, b)
 
-sess = tf.Session()
-init = tf.global_variables_initializer()
-sess.run(init)
-
-c_out = sess.run(c)
-d_out = sess.run(d)
-
-print(c_out)
-print(d_out)
+print(add_res)
+print(sub_res)
+print(mult_res)
+print(div_res)
