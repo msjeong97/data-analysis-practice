ee773aad428b6442d9101a9839daeaab6ee08186
msjeong97 <hpmltsws@gmail.com>
2021-05-19 15:34:35 +0900

    practice splitting train, test datat

--- sklearn_practice/california_housing_prices/practice.py
+++ sklearn_practice/california_housing_prices/practice.py
@@ -2,11 +2,35 @@ import os
 import urllib.request
 import tarfile
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import time
 
 
 dataset_url = 'https://raw.githubusercontent.com/ageron/handson-ml/master/'
 housing_path = os.path.join('../datasets', 'housing')
 housing_url = dataset_url + 'datasets/housing/housing.tgz'
+
+
+def main():
+	fetching_housing_data()
+	housing = load_housing_data()
+	# print(repr(housing.describe()))
+	# housing.hist(bins=50, figsize=(8, 6))
+	# plt.show()
+	# print(repr(housing['ocean_proximity'].value_counts()))
+	train, test = split_train_test(housing, 0.2)
+
+
+def split_train_test(data, ratio):
+	# generate random indexes from 0 to len(data)
+	shuffled_indices = np.random.permutation(len(data))
+	test_dataset_size = int(len(data) * ratio)
+	test_indices = shuffled_indices[:test_dataset_size]
+	train_indices = shuffled_indices[test_dataset_size:]
+
+	return data.iloc[train_indices], data.iloc[test_indices]
+	
 
 def fetching_housing_data(housing_url=housing_url, housing_path=housing_path):
 	if not os.path.isdir(housing_path):
@@ -24,13 +48,10 @@ def load_housing_data(housing_path=housing_path):
 	return pd.read_csv(csv_path)
 
 
-def main(): 
-	fetching_housing_data()
-	housing = load_housing_data()
-	print(repr(housing.head()))
-
-
 if __name__ == '__main__':
+	start_time = time.time()	
 	main()
+	end_time = time.time()
+	print("Time elapsed: %.2f sec" % (end_time - start_time))
 
 
